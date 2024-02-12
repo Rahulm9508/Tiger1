bce0635f8df2eec87c94c7748c523d7175037f09
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2024-02-12 18:52:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+API_ID = API_ID = 20346550
+
+API_HASH = os.environ.get("API_HASH", "bc79c3bea7a626887bdc0871eecf0327")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6548002778:AAHjrz6DmmOqftU1cUEX3fikmnSWlojrBrE")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 5665231556))
+
+LOG = -1002128466713
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "5665231556").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+ADMINS.append(OWNER)
+
+
